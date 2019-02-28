6abb1f956e40291bae4fe218c291e56ea8d08982
versatile-bug-8498 <dustin.clausing@gmail.com>
2019-02-28 18:02:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  famous_singers ("Cher", "Gaga", "Spears", "Celine")
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  famous_singers.push ("Cristina")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_singers.unshift ("lady")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  famous_singers.pop ("Cher")
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_singers
 end
 
 def retrieve_element_from_index(array, index_number)
